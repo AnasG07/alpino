d040c4f566d9d6f70bdcd1d8cf3b5fd5399a2054
Divyank Srivastava <divyank.srivastava@wesence.com>
2020-06-08 17:10:56 +0530

    fix: header

--- apps/storefront/components/Header.tsx
+++ apps/storefront/components/Header.tsx
@@ -3,26 +3,42 @@ import Link from 'next/link';
 
 export default function Header() {
   return (
-    <div header className="flex flex-row justify-between padding-30 items-center bg-black ">
+    <div className="flex flex-row justify-between padding-30 bg-black items-baseline">
       <img src="/logo.svg" alt="Alipno Logo" className="logo" />
-      <span className="flex items-center">
+      <span className="flex items-baseline">
+        <div className="subnav">
+          <Link href="/">
+            <a className="header-link px-4 text-right">
+              Earphones
+              <img src="./down-arrow.png" alt="arrow" className="inline-block h-3 px-2 " />
+            </a>
+          </Link>
+          <div className="subnav-content">
+            <Link href="/">
+              <a className="header-link px-4 block text-left">Neckbands</a>
+            </Link>
+            <Link href="/">
+              <a className="header-link px-4 block text-left">Headphones</a>
+            </Link>
+            <Link href="/">
+              <a className="header-link px-4 block text-left">Headsets- Single Ear</a>
+            </Link>
+            <Link href="/">
+              <a className="header-link px-4 block text-left">Wired earphones</a>
+            </Link>
+          </div>
+        </div>
         <Link href="/">
           <a className="header-link px-4">Speakers</a>
         </Link>
         <Link href="/">
-          <a className="header-link px-4">Headphones</a>
+          <a className="header-link px-4">Support</a>
         </Link>
         <Link href="/">
-          <a className="header-link px-4">Neckbands</a>
+          <a className="header-link px-4">Incubate with Alpino</a>
         </Link>
         <Link href="/">
-          <a className="header-link px-4">Earphones</a>
-        </Link>
-        <Link href="/">
-          <a className="header-link px-4">All Products</a>
-        </Link>
-        <Link href="/">
-          <a className="header-link px-4">More</a>
+          <a className="header-link px-4">Careers</a>
         </Link>
         <Link href="/">
           <a className="header-link px-4 inline-block">
--- apps/storefront/pages/index.tsx
+++ apps/storefront/pages/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import Head from 'next/head';
 import ProductFeature from '../components/ProductFeature.tsx';
 import ProductCard from '../components/ProductCard.tsx';
+import Header from '../components/Header.tsx';
+import Footer from '../components/Footer.tsx';
 
 export async function getServerSideProps(context) {
   return {
@@ -63,84 +65,88 @@ export async function getServerSideProps(context) {
 
 export default function Home({ productFeature, productCard }) {
   return (
-    <div className="min-h-screen bg-black">
-      <Head>
-        <title>Alpino</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main>
-        <div content className="pb-56">
-          {productFeature.map((i, index) => (
-            <ProductFeature data={i} index={index} />
-          ))}
-        </div>
-        <div content className="background-red-dark p-32">
-          <div className="flex flex-row">
-            <div className="flex-grow text-center">
-              <span className="card">animation</span>
+    <>
+      <Header />
+      <div className="min-h-screen bg-black">
+        <Head>
+          <title>Alpino</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <main>
+          <div className="pb-56">
+            {productFeature.map((i) => (
+              <ProductFeature data={i} />
+            ))}
+          </div>
+          <div className="background-red-dark p-32">
+            <div className="flex flex-row">
+              <div className="flex-grow text-center">
+                <span className="card">animation</span>
+              </div>
+              <div className="flex-grow flex flex-col justify-center max-width-50">
+                <h1 className="text-black text-left max-w-30">
+                  Your ideas are worthy of so much more than the kudos in your imagination.
+                </h1>
+                <div className="pt-64">
+                  <button className="button-transparent py-4 px-8 border-none bg-black outline-none">
+                    Incubate with Alpino
+                  </button>
+                </div>
+              </div>
             </div>
-            <div className="flex-grow flex flex-col justify-center max-width-50">
-              <h1 className="text-black text-left max-w-30">
-                Your ideas are worthy of so much more than the kudos in your imagination.
-              </h1>
-              <div className="pt-64">
-                <button className="button-transparent py-4 px-8 border-none bg-black outline-none">
-                  Incubate with Alpino
+          </div>
+          <div className="pt-32">
+            <div className="flex flex-row justify-between items-center px-30">
+              <h1>Best Sellers</h1>
+              <h3>Best always deserves better</h3>
+            </div>
+            <div className="flex flex-row pt-32 justify-between pl-30 pr-10 items-center">
+              {productCard.map((i) => (
+                <ProductCard data={i} />
+              ))}
+              <span>
+                <button className="border-none outline-none">
+                  <img className="inline-block" src="arrow-right.svg" />
                 </button>
+              </span>
+            </div>
+          </div>
+          <div className="background-image-people p-32 bg-no-repeat mt-32 bg-cover">
+            <div className="flex flex-row">
+              <div className="flex-grow">
+                <h1 className="text-left">About us </h1>
+              </div>
+              <div className="flex-grow flex flex-col justify-center max-width-50">
+                <h3 className="text-left max-w-45">
+                  We refuse to conform to the idea of being just a consumer electronics brand, and instead believe we’re
+                  a lifestyle brand. As a fast-paced company, we make sure our products help you listen to the voice of
+                  righteousness and block out the noise of negativity.
+                </h3>
+                <div className="pt-32">
+                  <button className="button-transparent py-2 px-8 outline-none">Learn More</button>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        <div content className="pt-32">
-          <div className="flex flex-row justify-between items-center px-30">
-            <h1>Best Sellers</h1>
-            <h3>Best always deserves better</h3>
-          </div>
-          <div className="flex flex-row pt-32 justify-between pl-30 pr-10 items-center">
-            {productCard.map((i) => (
-              <ProductCard data={i} />
-            ))}
-            <span>
-              <button className="border-none outline-none">
-                <img className="inline-block" src="arrow-right.svg" />
-              </button>
-            </span>
-          </div>
-        </div>
-        <div content className="background-image-people p-32 bg-no-repeat mt-32">
-          <div className="flex flex-row">
-            <div className="flex-grow">
-              <h1 className="text-left">About us </h1>
+          <div className="pt-32">
+            <div className="flex flex-row justify-between items-center px-30">
+              <h1>Sneak Peak</h1>
+              <h3>The future before it happens</h3>
             </div>
-            <div className="flex-grow flex flex-col justify-center max-width-50">
-              <h3 className="text-left max-w-45">
-                We refuse to conform to the idea of being just a consumer electronics brand, and instead believe we’re a
-                lifestyle brand. As a fast-paced company, we make sure our products help you listen to the voice of
-                righteousness and block out the noise of negativity.
-              </h3>
-              <div className="pt-32">
-                <button className="button-transparent py-2 px-8 outline-none">Learn More</button>
-              </div>
+            <div className="flex flex-row pt-32 justify-between pl-30 pr-10 items-center">
+              {productCard.map((i) => (
+                <ProductCard data={i} />
+              ))}
+              <span>
+                <button className="border-none outline-none">
+                  <img className="inline-block" src="arrow-right.svg" />
+                </button>
+              </span>
             </div>
           </div>
-        </div>
-        <div content className="pt-32">
-          <div className="flex flex-row justify-between items-center px-30">
-            <h1>Sneak Peak</h1>
-            <h3>The future before it happens</h3>
-          </div>
-          <div className="flex flex-row pt-32 justify-between pl-30 pr-10 items-center">
-            {productCard.map((i) => (
-              <ProductCard data={i} />
-            ))}
-            <span>
-              <button className="border-none outline-none">
-                <img className="inline-block" src="arrow-right.svg" />
-              </button>
-            </span>
-          </div>
-        </div>
-      </main>
-    </div>
+        </main>
+      </div>
+      <Footer />
+    </>
   );
 }
