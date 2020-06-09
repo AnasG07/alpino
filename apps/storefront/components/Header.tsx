fb98f7808cb3c27283acfe9b2e436578e163d1e2
Divyank Srivastava <divyank.srivastava@wesence.com>
2020-06-09 16:23:23 +0530

    feat: support page

--- apps/storefront/components/Header.tsx
+++ apps/storefront/components/Header.tsx
@@ -1,52 +1,60 @@
 import React from 'react';
 import Link from 'next/link';
 
-export default function Header() {
+export default function Header({ invert }) {
   return (
-    <div className="flex flex-row justify-between padding-30 bg-black items-baseline">
-      <img src="/logo.svg" alt="Alipno Logo" className="logo" />
+    <div className="flex flex-row justify-between padding-30 items-baseline">
+      <img src="/logo.svg" alt="Alipno Logo" className={`logo ${invert && 'filter-invert'}`} />
       <span className="flex items-baseline">
         <div className="subnav">
           <Link href="/">
-            <a className="header-link px-4 text-right">
+            <a className={`header-link px-4 text-right ${invert ? 'text-black' : 'text-white'}`}>
               Earphones
-              <img src="./down-arrow.png" alt="arrow" className="inline-block h-3 px-2 " />
+              <img
+                src="./down-arrow.png"
+                alt="arrow"
+                className={`inline-block h-3 px-2 ${invert && 'filter-invert'}`}
+              />
             </a>
           </Link>
           <div className="subnav-content">
             <Link href="/">
-              <a className="header-link px-4 block text-left">Neckbands</a>
+              <a className={`header-link px-4 block text-left ${invert ? 'text-black' : 'text-white'}`}>Neckbands</a>
             </Link>
             <Link href="/">
-              <a className="header-link px-4 block text-left">Headphones</a>
+              <a className={`header-link px-4 block text-left ${invert ? 'text-black' : 'text-white'}`}>Headphones</a>
             </Link>
             <Link href="/">
-              <a className="header-link px-4 block text-left">Headsets- Single Ear</a>
+              <a className={`header-link px-4 block text-left ${invert ? 'text-black' : 'text-white'}`}>
+                Headsets- Single Ear
+              </a>
             </Link>
             <Link href="/">
-              <a className="header-link px-4 block text-left">Wired earphones</a>
+              <a className={`header-link px-4 block text-left ${invert ? 'text-black' : 'text-white'}`}>
+                Wired earphones
+              </a>
             </Link>
           </div>
         </div>
         <Link href="/">
-          <a className="header-link px-4">Speakers</a>
+          <a className={`header-link px-4 text-right ${invert ? 'text-black' : 'text-white'}`}>Speakers</a>
         </Link>
         <Link href="/">
-          <a className="header-link px-4">Support</a>
+          <a className={`header-link px-4 text-right ${invert ? 'text-black' : 'text-white'}`}>Support</a>
         </Link>
         <Link href="/">
-          <a className="header-link px-4">Incubate with Alpino</a>
+          <a className={`header-link px-4 text-right ${invert ? 'text-black' : 'text-white'}`}>Incubate with Alpino</a>
         </Link>
         <Link href="/">
-          <a className="header-link px-4">Careers</a>
+          <a className={`header-link px-4 text-right ${invert ? 'text-black' : 'text-white'}`}>Careers</a>
         </Link>
         <Link href="/">
-          <a className="header-link px-4 inline-block">
+          <a className={`header-link px-4 inline-block ${invert && 'filter-invert'}`}>
             <img src="/user.svg" />
           </a>
         </Link>
         <Link href="/">
-          <a className="header-link px-4 inline-block">
+          <a className={`header-link px-4 inline-block ${invert && 'filter-invert'}`}>
             <img src="/cart.svg" />
           </a>
         </Link>
