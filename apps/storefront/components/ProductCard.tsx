bca5af7b5d225dc1915c88c5f9780a618daafcca
Divyank Srivastava <divyank.srivastava@wesence.com>
2020-06-26 14:31:49 +0530

    feat: index , contact , blogs

--- apps/storefront/components/ProductCard.tsx
+++ apps/storefront/components/ProductCard.tsx
@@ -16,22 +16,22 @@ export default function ProductCard({
         <img src={image} alt="Rock bluetooth" />
         <div className="flex flex-row items-center  justify-between">
           <div>
-            <h3 className="max-w-14 leading-5 text-left text-white font-normal text-xl">{name}</h3>
-            <h3 className="max-w-14 leading-5 text-left text-white font-normal text-xl">{type}</h3>
+            <h3 className="max-w-14 leading-5 text-left text-white font-normal text-sm md:text-xl">{name}</h3>
+            <h3 className="max-w-14 leading-5 text-left text-white font-normal text-sm md:text-xl">{type}</h3>
           </div>
-          <span className="text-white text-base">
+          <span className="text-white text-xs md:text-base">
             <img className="inline-block h-6" src="indian.png" />
             {price}
           </span>
         </div>
         <div className="flex flex-row justify-between items-center pt-10 hover-show">
           <Link href="/products/[slug]" as={`/products/${slug}`}>
-            <a className="leading-5 rounded-full py-3 text-base px-8 outline-none border-none bg-black max-w-8 w-full flex justify-center text-white">
+            <a className="leading-5 rounded-full py-3 text-xs md:text-base px-8 outline-none border-none bg-black max-w-8 w-full flex justify-center text-white">
               Details
             </a>
           </Link>
           {!comingSoon && (
-            <button className="leading-5 rounded-full text-base py-3 px-8 outline-none border-none bg-white text-black  max-w-8 w-full flex justify-center">
+            <button className="leading-5 rounded-full text-xs md:text-base py-3 px-8 outline-none border-none bg-white text-black  max-w-8 w-full flex justify-center">
               {buttonText}
             </button>
           )}
