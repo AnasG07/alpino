0a2d7bf54fde309a472eb7ceb3e427afb2d53d99
Ashish Agrawal <ashish@wesence.com>
2020-06-29 23:46:08 +0530

    fix: fix in the category card

--- apps/storefront/components/CategoryCard.tsx
+++ apps/storefront/components/CategoryCard.tsx
@@ -6,12 +6,16 @@ import Link from 'next/link';
 export default function CategoryCard({ data, index }) {
   return (
     <div key={index}>
-      <Link href="/collections/[tag]" as="/collections/[tag]">
+      <Link href="/collections/[tag]" as={`/collections/${data.slug}`}>
         <div className={classNames(style.container, 'border-none cursor-pointer flex items-center justify-center')}>
-          <img src={data.image} className></img>
+          <img src={data.image} />
         </div>
       </Link>
-      <h2 className="text-2xl md:text-4xl text-white text-center mt-4 font-medium">{data.title}</h2>
+      <h2 className="text-2xl md:text-4xl text-white text-center mt-4 font-medium">
+        <Link href="/collections/[tag]" as={`/collections/${data.slug}`}>
+          {data.title}
+        </Link>
+      </h2>
       <h3 className="text-sm md:text-base text-white text-center mt-6 mb-32">{data.description}</h3>
     </div>
   );
